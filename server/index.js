1e2f74993c7943c5176c7e7d1eeabe8352df0c7c
thomas-rooty <tc.caron91@gmail.com>
2024-01-07 17:48:10 +0100

    Socket.IO connection when landing on planet

--- server/index.js
+++ server/index.js
@@ -8,12 +8,31 @@ const io = new Server({
 
 io.listen(3001)
 
+const astronauts = []
+
+const generateRandomPosition = () => {
+  return [Math.random() * 3, 0, Math.random() * 3]
+}
+
+const generateRandomHexColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
 io.on('connection', (socket) => {
   console.log('a user connected')
 
-  socket.emit('hello')
+  astronauts.push({
+    id: socket.id,
+    position: generateRandomPosition(),
+    headColor: generateRandomHexColor()
+  })
+
+  io.emit('astronauts', astronauts)
 
   socket.on('disconnect', () => {
-    console.log('a user disconnected')
+    console.log('user disconnected')
+    // Remove astronaut from array and emit new array to all clients
+    astronauts.splice(astronauts.findIndex(astronaut => astronaut.id === socket.id), 1)
+    io.emit('astronauts', astronauts)
   })
 })
