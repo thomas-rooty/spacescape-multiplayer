af6987adf5e1109d4aaa428b179e9d9f7d22f214
thomas-rooty <tc.caron91@gmail.com>
2024-01-08 21:06:17 +0100

    Edited CORS

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://spacescape.vercel.app"],
   },
 });
 
