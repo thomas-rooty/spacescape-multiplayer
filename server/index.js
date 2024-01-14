e5ea725da7fc480dd377fe530cfa095a8513530f
thomas-rooty <tc.caron91@gmail.com>
2024-01-14 13:55:54 +0100

    Mapping astronauts adds O(1) time complexity, added throttling system (disabled), better height placement

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const io = new Server({
 
 io.listen(process.env.PORT || 3001);
 
-const astronauts = [];
+const astronauts = new Map(); // Using Map for O(1) access
+const updateFrequency = 1; // Frequency of updates in milliseconds
 
 const generateRandomPosition = () => {
-  // I want it between ['-0.45', '0.07', '24.37'] and ['0.46', '0.07', '25.37']
   const x = Math.random() * (0.46 - -0.45) + -0.45;
   const y = 0.07;
   const z = Math.random() * (25.37 - 24.37) + 24.37;
@@ -25,42 +25,37 @@ const generateRandomHexColor = () => {
 io.on("connection", (socket) => {
   console.log("User ID: " + socket.id + " connected");
 
-  astronauts.push({
+  astronauts.set(socket.id, {
     id: socket.id,
     position: generateRandomPosition(),
     headColor: generateRandomHexColor(),
-    animation: 'CharacterArmature|Idle',
-    lookingAt: {
-      x: 0,
-      y: 0,
-      z: 31
-    },
+    animation: "CharacterArmature|Idle",
+    lookingAt: { x: 0, y: 0, z: 31 },
   });
 
-  io.emit("astronauts", astronauts);
-
-  // Listen for astronaut position updates
+  // Throttle updates
+  let lastUpdateTime = Date.now();
   socket.on("move", (data) => {
-    const astronaut = astronauts.find((a) => a.id === socket.id);
-    if (
-      astronaut &&
-      Array.isArray(data.newPosition) &&
-      data.newPosition.length === 3
-    ) {
-      astronaut.position = data.newPosition;
-      astronaut.animation = data.animation;
-      astronaut.lookingAt = data.lookingAt;
-      io.emit("astronauts", astronauts);
+    const now = Date.now();
+    if (now - lastUpdateTime > updateFrequency) {
+      const astronaut = astronauts.get(socket.id);
+      if (
+        astronaut &&
+        Array.isArray(data.newPosition) &&
+        data.newPosition.length === 3
+      ) {
+        astronaut.position = data.newPosition;
+        astronaut.animation = data.animation;
+        astronaut.lookingAt = data.lookingAt;
+        io.emit("astronauts", Array.from(astronauts.values()));
+        lastUpdateTime = now;
+      }
     }
   });
 
   socket.on("disconnect", () => {
     console.log("User ID: " + socket.id + " disconnected");
-    // Remove astronaut from array and emit new array to all clients
-    astronauts.splice(
-      astronauts.findIndex((astronaut) => astronaut.id === socket.id),
-      1
-    );
-    io.emit("astronauts", astronauts);
+    astronauts.delete(socket.id);
+    io.emit("astronauts", Array.from(astronauts.values()));
   });
 });
